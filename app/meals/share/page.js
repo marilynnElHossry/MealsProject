5a80b62eb236149a53db867513965bf994eba3b5
marilynnElHossry <marilynnelhossry@gmail.com>
2024-04-01 23:49:33 +0300

    server action file save xss and slugify redirect

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
+import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/mealsFormSubmit';
 export default function ShareMealPage() {
     return (
         <>
@@ -10,7 +12,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={ classes.main }>
-                <form className={ classes.form }>
+                <form className={ classes.form } action={ shareMeal }>
                     <div className={ classes.row }>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -39,9 +41,13 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
 
-                    <ImagePicker />
+                    <ImagePicker
+                        label="Your image"
+                        name="image"
+
+                    />
                     <p className={ classes.actions }>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
